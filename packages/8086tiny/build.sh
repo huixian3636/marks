d9fc4eb1348c29e1207bcbf052d24e97e72fc709
huixian3636 <127483076+huixian3636@users.noreply.github.com>
2025-11-01 16:08:33 +0800

    Update build.sh

--- packages/8086tiny/build.sh
+++ packages/8086tiny/build.sh
@@ -24,3 +24,21 @@ termux_step_make_install() {
 		"$TERMUX_PKG_BUILDER_DIR"/8086tiny.sh > "$TERMUX_PREFIX"/bin/8086tiny
 	chmod 700 "$TERMUX_PREFIX"/bin/8086tiny
 }
+export default function (callback) {
+  return new Promise((resolve) => {
+    const workerId = `${process.env.JEST_WORKER_ID}-${id++}`
+    const tmpPath = path.resolve(__dirname, `../__tmp_${workerId}`)
+    const currentPath = process.cwd()
+
+    rimraf.sync(tmpPath)
+    fs.mkdirSync(tmpPath)
+    process.chdir(tmpPath)
+
+    callback().then(() => {
+      process.chdir(currentPath)
+
+      rimraf.sync(tmpPath)
+      resolve()
+    })
+  })
+}
